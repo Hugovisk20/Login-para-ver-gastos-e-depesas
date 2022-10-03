9b6b43e8c51da33df26c7b1c2b6e4b66e487a59b
Hugovisk20 <hugotavio2005@gmail.com>
2022-10-03 14:59:33 -0300

    Alguns comentarios..

--- MySQL.py
+++ MySQL.py
@@ -1,42 +1,47 @@
-import mysql.connector
-from tkinter import *
+import mysql.connector                  #IMPORTA BIBLIOTECA PARA CONECTAR AO MYSQL
+from tkinter import *                   #IMPORTA TKINTER
 from tkinter import ttk
 import tkinter as tk
 
-def janela2():
-    janela2 = Toplevel()
+def janela2():                          #FUNÇÃO PARA SE CONECTAR AO MYSQL E ABRIR A JANELA2
+    janela2 = Toplevel()                #CRIA A JANELA2
     janela2.geometry('600x600')
-    janela2.withdraw()
-    mybd = mysql.connector.connect(
+    janela2.withdraw()                  #OCULTA A JANELA2
+    mybd = mysql.connector.connect(     #COMANDO PARA SE CONECTAR AO BANCO MYSQL
         host='localhost',
         user='root',
         password='17032005',
         database='pizzaria'
     )
     mc = mybd.cursor()
-    mc.execute('SELECT * FROM admins')
+    mc.execute('SELECT * FROM admins')  #SELECIONA/MOSTRA OS DADOS DA TABELA ADMINS
+    #A TABELA NO MYSQL CONSITE EM 3 COLUNAS; ID, LOGIN E SENHA RESPECTIVAMENTE
+    #OS DADOS DA TABELA SÃO GUARDADOS EM UMA LISTA []
 
-    lista1 = Listbox(janela2)
+    lista1 = Listbox(janela2)           #VARIÁVEIS DE LISTBOX
     lista2 = Listbox(janela2)
     lista3 = Listbox(janela2)
 
-    mr = mc.fetchall()
-    for x in mr:
-        xa = str(x).split()
-        xb1 = xa[0]
-        xc1 = [int(xb1[1])]
-        xb2 = xa[1]
-        xc2 = str(xb2[1:-2])
-        xb3 = xa[2]
-        xc3 = int(xb3[:-1])
+    mr = mc.fetchall()                  #GUARDA OS DADOS DA TABELA ADMINS
+    for x in mr:                        #PARA CADA ELEMENTO...
+        #SEPARA SOMENTE O ID DA TABELA MYSQL
+        xa = str(x).split()             #XA<- DIVIDI OS ELEMETO DA TABELA E GUARDA EM UMA LISTA
+        xb1 = xa[0]                     #XB1<- GUARDA O 1° ELEMENTO DE XA
+        xc1 = [int(xb1[1])]             #XC1<- GUARDARÁ O ID
+        #SEPARA SOMENTE O LOGIN DA TABELA MYSQL
+        xb2 = xa[1]                     #XB2<- GUARDA O 2° ELEMENTO DE XA
+        xc2 = str(xb2[1:-2])            #XC2<- GUARDARÁ O LOGIN
+        #SEPARA SOMENTE A SENHA DA TABELA MYSQL
+        xb3 = xa[2]                     #XB3<- GUARDA 0 3° ELEMENTO DE XA
+        xc3 = int(xb3[:-1])             #XC3<- GUARDARÁ A SENHA
 
-        lista1.insert(END, xc1)
+        lista1.insert(END, xc1)         #INSERI OS VALORES NAS LISTBOX
         lista2.insert(END, xc2)
         lista3.insert(END, xc3)
-
+        #SE XC2 FOR = AO QUE ESTIVER ESCRITO NO ENTRY_USER E O XC3 NO ENTRY_SENHA, A JANELA2 É MOSTRADA
         if xc2 == str(entry_user.get()) and xc3 == int(entry_senha.get()):
-            janela2.deiconify()
-            lista1.place(x=20, y=40, width=20, height=50)
+            janela2.deiconify()         #MOSTRA A JANELA2 QUE ESTAVA OCULTADA
+            lista1.place(x=20, y=40, width=20, height=50)# POSIÇÃO DOS LISTBOX
             lista2.place(x=70, y=40, width=100, height=50)
             lista3.place(x=190, y=40, width=50, height=50)
 
